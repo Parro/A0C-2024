1563a129a63e9345bb38f8e81654020478e12422
Parro <29497+Parro@users.noreply.github.com>
2024-12-03 11:04:37 +0100

    feat: Add day 1 second exercise solution

--- src/day-1/second.test.ts
+++ src/day-1/second.test.ts
@@ -5,9 +5,14 @@ import { main } from './second';
 
 describe('Day 1 second exercise tests', () => {
   it('should test day 1 second exercise', () => {
-    const input = '';
+    const input = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
 
-    const expectedOutput = '';
+    const expectedOutput = 31;
 
     const output = main(input);
 
--- src/day-1/second.ts
+++ src/day-1/second.ts
@@ -1,7 +1,36 @@
-type Main = (input: string) => string;
+import getColumns from './getColumns';
+
+type Main = (input: string) => number;
 
 const main: Main = (input) => {
-  return input;
+  const rows = input.split('\n');
+  // console.log('🚀 ~ rows:', rows);
+
+  const columns = getColumns(rows);
+
+  // console.log('🚀 ~ columns:', columns);
+
+  const secondColumnsCount: { [key: number]: number } = {};
+
+  const { first, second } = columns;
+
+  return first.reduce((acc, number) => {
+    if (secondColumnsCount[number] === undefined) {
+      const count = second.reduce((acc, secondNumber) => {
+        if (secondNumber === number) {
+          acc = acc + 1;
+        }
+        return acc;
+      }, 0);
+
+      secondColumnsCount[number] = count;
+    }
+    // console.log('🚀 ~ secondColumnsCount:', secondColumnsCount);
+
+    acc += number * secondColumnsCount[number];
+
+    return acc;
+  }, 0);
 };
 
 export { main };
